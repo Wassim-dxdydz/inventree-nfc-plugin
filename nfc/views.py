f85848e2fec58fb1926dd07d709fac4f2ffb7e5b
Wassim-dxdydz <abahriwassim44@gmail.com>
2026-04-15 12:09:42 +0000

    NFC Scan view

--- nfc/views.py
+++ nfc/views.py
@@ -1,4 +1,5 @@
 """
+API Views for the NFC plugin
 """
 
 from rest_framework.permissions import IsAuthenticated
@@ -7,11 +8,27 @@ from rest_framework.views import APIView
 
 class NFCScanStatusView(APIView):
     """
+    GET - Returns the last scanned NFC UID and clears it.
+    The frontend polls this every second while scanning is active
     """
 
-    # You can control which users can access this view using DRF permissions
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
         """
+        GET the last scanned UID
         """
+
+        from .core import NFC
+        with NFC._lock:
+            uid = NFC._last_scanned_uid
+            NFC._last_scanned_uid = None
+        if uid:
+            return Response({
+                "found": True,
+                "uid": uid,
+            })
+        return Response({
+            "found": False,
+            "uid": None
+        })
