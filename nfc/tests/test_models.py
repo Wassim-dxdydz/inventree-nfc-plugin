d4f134dedad0a26cbfb82966ede00a668e317a7d
Wassim-dxdydz <abahriwassim44@gmail.com>
2026-05-18 14:11:08 +0000

    model test 4

--- nfc/tests/test_models.py
+++ nfc/tests/test_models.py
@@ -22,3 +22,13 @@ def test_str_representation(nfc_link_factory):
     """__str__ should return 'NFC <uid> : <part name>'."""
     link = nfc_link_factory(uid="AABBCCDD")
     assert str(link) == f"NFC AABBCCDD : {link.part.name}"
+
+@pytest.mark.django_db
+def test_unique_active_uid_constraint(nfc_link_factory, part_factory):
+    """Two active links with the same UID must be rejected."""
+    from django.db import IntegrityError
+    part1 = part_factory(name="Part A")
+    part2 = part_factory(name="Part B")
+    nfc_link_factory(uid="AABBCCDD", part=part1, active=True)
+    with pytest.raises(IntegrityError):
+        nfc_link_factory(uid="AABBCCDD", part=part2, active=True)
