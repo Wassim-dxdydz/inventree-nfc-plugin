05aafa9d41a0c5871a60ec750faf31cd78ed7662
Wassim-dxdydz <abahriwassim44@gmail.com>
2026-05-18 13:48:37 +0000

    model test 2

--- nfc/tests/test_models.py
+++ nfc/tests/test_models.py
@@ -10,3 +10,9 @@ def test_uid_is_uppercased_on_save(nfc_link_factory):
     """UID must be stored as uppercase regardless of input."""
     link = nfc_link_factory(uid="aabbccdd")
     assert link.uid == "AABBCCDD"
+
+@pytest.mark.django_db
+def test_uid_is_stripped_on_save(nfc_link_factory):
+    """Leading/trailing whitespace must be stripped from UID."""
+    link = nfc_link_factory(uid="  AABBCCDD  ")
+    assert link.uid == "AABBCCDD"
